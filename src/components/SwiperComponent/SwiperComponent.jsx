8a2da8a62165b4fe4a1f113af1e72a0ebf35b06d
Михаил <shonovgacha@gmail.com>
2025-04-02 22:06:10 +0300

    skills4

--- src/components/SwiperComponent/SwiperComponent.jsx
+++ src/components/SwiperComponent/SwiperComponent.jsx
@@ -2,7 +2,33 @@ import { swiperEls, colors } from '../../helpers/data'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-import img from '../../img/skills/html.svg'
+import html from '../../img/skills/html.svg'
+import css from '../../img/skills/css.svg'
+import js from '../../img/skills/js.svg'
+import react from '../../img/skills/react.svg'
+import sass from '../../img/skills/sass.svg'
+import redux from '../../img/skills/redux.svg'
+import next from '../../img/skills/next.svg'
+import vite from '../../img/skills/vite.svg'
+import webpack from '../../img/skills/webpack.svg'
+import git from '../../img/skills/git.svg'
+import github from '../../img/skills/github.svg'
+import figma from '../../img/skills/figma.svg'
+
+const arr = [
+    html,
+    css,
+    js,
+    react,
+    sass,
+    redux,
+    next,
+    vite,
+    webpack,
+    git,
+    github,
+    figma,
+]
 
 import 'swiper/css'
 import 'swiper/css/navigation'
@@ -26,7 +52,7 @@ export default function SwiperComponent(props) {
                         style={{ color: colors[index] }}
                         key={swiperEl.id}
                     >
-                        <SlideElement id={swiperEls.id} img={img} />
+                        <SlideElement id={swiperEl.id} img={arr[index]} />
                     </SwiperSlide>
                 ))}
             </Swiper>
