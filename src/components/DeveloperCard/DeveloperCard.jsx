b04f731ae6a523dcf746f3f4a3e342e7c28aef18
Михаил <shonovgacha@gmail.com>
2025-04-15 15:24:43 +0300

    resume3

--- src/components/DeveloperCard/DeveloperCard.jsx
+++ src/components/DeveloperCard/DeveloperCard.jsx
@@ -6,7 +6,7 @@ import cardLocationImg from '../../img/promo/promo-location.svg'
 import cardWorkImg from '../../img/promo/promo-work.svg'
 import cardDownloadImg from '../../img/promo/promo-download.svg'
 
-import resume from '../../img/front.pdf'
+import resume from '../../img/portfolio.pdf'
 
 export default function DeveloperCard() {
     return (
